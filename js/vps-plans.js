59a0ba1474d65af45f22100c13e8f8ec774e3551
joshua66553 <joshua66553@gmail.com>
2020-08-01 23:11:24 +0200

    first commit

---
+++ js/vps-plans.js
@@ -0,0 +1,64 @@
+/*
+
+[Linux VPS Plans Script]
+
+Project: ColorHost - Responsive HTML5 Web Hosting and WHMCS Template
+Version: 1.0
+Author : themelooks.com
+
+*/
+
+'use strict';
+
+// VPS slider options [Note: you can edit these options]
+var vpsSliderOpts = {
+    "detfaultPlan": "1",
+    "maxPlans": "5",
+    "plans": [
+	    {
+            "planName": "Dirt",
+            "cpuText": "Unlimited",
+            "ramText": "512 MB",
+            "spaceText": "5 GB",
+            "brandwidthText": "Unlimited",
+            "priceText": "$1.50",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?a=add&pid=125"
+        },
+        {
+            "planName": "Redstone",
+            "cpuText": "Unlimited",
+            "ramText": "1 GB",
+            "spaceText": "10 GB",
+            "brandwidthText": "Unlimited",
+            "priceText": "$4.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?a=add&pid=120"
+        },
+        {
+            "planName": "Diamond",
+            "cpuText": "Unlimited",
+            "ramText": "3 GB",
+            "spaceText": "20 GB",
+            "brandwidthText": "Unlimited",
+            "priceText": "$9.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?a=add&pid=126"
+        },
+        {
+            "planName": "Emerald",
+            "cpuText": "Unlimited",
+            "ramText": "6 GB",
+            "spaceText": "40 GB",
+            "brandwidthText": "Unlimited",
+            "priceText": "$18.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?a=add&pid=127"
+        },
+        {
+            "planName": "Bedrock",
+            "cpuText": "Unlimited",
+            "ramText": "12 GB",
+            "spaceText": "100 GB",
+            "brandwidthText": "Unlimited",
+            "priceText": "$36.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?a=add&pid=128"
+        }
+    ]
+};
---
+++ js/wvps-plans.js
@@ -0,0 +1,55 @@
+/*
+
+[Windows VPS Plans Script]
+
+Project: ColorHost - Responsive HTML5 Web Hosting and WHMCS Template
+Version: 1.0
+Author : themelooks.com
+
+*/
+
+'use strict';
+
+// VPS slider options [Note: you can edit these options]
+var vpsSliderOpts = {
+    "detfaultPlan": "2",
+    "maxPlans": "4",
+    "plans": [
+        {
+            "planName": "Starter Plan",
+            "cpuText": "1 CORE",
+            "ramText": "3 GB",
+            "spaceText": "25 GB",
+            "brandwidthText": "1 Gbit/s",
+            "priceText": "$10.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?gid=12"
+        },
+        {
+            "planName": "Pro Plan",
+            "cpuText": "2 CORE",
+            "ramText": "4 GB",
+            "spaceText": "60 GB",
+            "brandwidthText": "1 Gbit/s",
+            "priceText": "$18.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?gid=12"
+        },
+        {
+            "planName": "Premium Plan",
+            "cpuText": "4 CORE",
+            "ramText": "8 GB",
+            "spaceText": "100 GB",
+            "brandwidthText": "1 Gbit/s",
+            "priceText": "$34.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?gid=12"
+        },
+        {
+            "planName": "Ulitmate Plan",
+            "cpuText": "6 CORE",
+            "ramText": "16 GB",
+            "spaceText": "250 GB",
+            "brandwidthText": "1 Gbit/s",
+            "priceText": "$45.00",
+            "url": "https://shopmodernsolutions.com/billing/cart.php?gid=12"
+        }
+    ]
+};
